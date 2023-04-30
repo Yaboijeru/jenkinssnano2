5181f8dab0bb36d73c0d6de1612816a2ae47d77e
Yaboijeru <56378201+Yaboijeru@users.noreply.github.com>
2023-04-30 20:01:42 +0700

    Create inupdatedphp.php

---
+++ inupdatedphp.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "172.17.0.2:3306";
+$username = "root";
+$password = "ayampanggang";
+$dbname = "Redlock";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT COUNT(*) FROM users";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+    echo $row;
+}
+
+$conn->close();
+?>
+
